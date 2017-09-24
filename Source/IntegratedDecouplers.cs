c042f2c4255f56c38b9d74baa7113027c3cb279a
Jonathan Bayer <linuxgurugamer@gmail.com>
2017-09-24 14:00:23 -0400

    Initial beta release

---
+++ Source/IntegratedDecouplers.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using KSP.UI.Screens;
+
+
+namespace IntegratedStackedTankDecouplers
+{
+    public class IntegratedDecoupler : ModuleToggleCrossfeed, IPartMassModifier, IPartCostModifier
+    {
+        [KSPField(isPersistant = true, guiActiveEditor = false, guiActive = false)]
+        public bool integratedDecoupler = false;
+
+        [KSPEvent(name= "integratedDecoupler", guiName = "No decoupler", guiActiveEditor = true, active = true)]
+        public void ToggleIntegratedDecoupler()
+        {
+            integratedDecoupler = !integratedDecoupler;
+            SetStatus();           
+        }
+
+//        [KSPField(guiName = "Seperatron Count", isPersistant = true, guiActiveEditor = true),
+//            UI_FloatRange(stepIncrement = 1f, maxValue = 100f, minValue = 0f)]
+        public float seperatronCnt = 0;
+        float oldSepCnt;
+
+        [KSPField(isPersistant = true, guiActive = false, guiActiveEditor = false)]
+        public float decouplerMass = 0.15f;
+
+        [KSPField(isPersistant = true, guiActive = false, guiActiveEditor = false)]
+        public float decouplerCost = 250f;
+
+        [KSPField(isPersistant = true, guiActive = false, guiActiveEditor = false)]
+        public new string techRequired = "";
+
+        NodeControl newNodes;
+
+
+        void SetStatus()
+        {
+            if (integratedDecoupler)
+            {
+                enableDecoupler();
+            }
+            else
+            {
+                disableDecoupler();
+            }
+            crossfeedToggleModule.Events["ToggleEvent"].guiActive = false;
+            crossfeedToggleModule.Events["ToggleEvent"].active = false;
+            crossfeedToggleModule.Actions["ToggleAction"].active = false;
+            crossfeedToggleModule.Actions["EnableAction"].active = false;
+            crossfeedToggleModule.Actions["DisableAction"].active = false;
+
+            if (decouplerModule != null)
+               part.UpdateStageability(true, true);
+            
+            GameEvents.onEditorShipModified.Fire(EditorLogic.fetch.ship);
+        }
+
+        void enableDecoupler()
+        {
+            KSPActionParam kap = null;
+            base.enabled = true;
+            if (decouplerModule != null)
+            {
+                decouplerModule.SetStaging(true);
+                decouplerModule.Events["ToggleStaging"].guiActiveEditor = true;
+                decouplerModule.Fields["ejectionForcePercent"].guiActiveEditor = true;
+                stagingEnabled = true;
+                crossfeedToggleModule.DisableAction(kap);
+            }
+
+            newNodes = new NodeControl(this.part, (int)seperatronCnt);
+            
+
+            Events["ToggleIntegratedDecoupler"].guiName = "Integrated Decoupler";
+
+        }
+        void disableDecoupler()
+        {
+            KSPActionParam kap = null;
+
+            if (decouplerModule != null)
+            {
+                decouplerModule.SetStaging(false);
+                decouplerModule.Events["ToggleStaging"].guiActiveEditor = false;
+                decouplerModule.Fields["ejectionForcePercent"].guiActiveEditor = false;
+
+                stagingEnabled = false;
+
+                crossfeedToggleModule.EnableAction(kap);
+            }
+            base.enabled = false;
+            Events["ToggleIntegratedDecoupler"].guiName = "No decoupler";
+            if (newNodes != null)
+                newNodes.DelNodes(this.part);
+        }
+        ModuleDecouple decouplerModule = null;
+        PartModule decouplerPartModule = null;
+        ModuleToggleCrossfeed crossfeedToggleModule = null;
+
+        bool techAvailable { get
+        {
+                var b = true;
+                if (this.DecouplerRequiresTech())
+                    b = this.DecouplerHasTech();
+                return b;
+            }
+        }
+        public override void OnStart(StartState state)
+        {
+            Log.Info("IntegratedDecoupler.OnStart");
+            base.OnStart(state);
+
+
+            decouplerModule = this.part.FindModuleImplementing<ModuleDecouple>();
+            decouplerPartModule = decouplerModule;
+
+            crossfeedToggleModule = this.part.FindModuleImplementing<ModuleToggleCrossfeed>();
+
+            ModuleJettison moduleJettison = this.part.FindModuleImplementing<ModuleJettison>();            
+
+            if (decouplerModule == null)
+                Log.Info("ModuleDecouple NOT found");
+            else
+                Log.Info("ModuleDecouple found");
+
+            if (!techAvailable || moduleJettison != null)
+            {
+                this.Events["ToggleIntegratedDecoupler"].guiActiveEditor = false;
+                // this.Fields["seperatronCnt"].guiActiveEditor = false;
+                Log.Info("Disabling IntegratedDecoupler due to no tech");
+                disableDecoupler();
+                if (decouplerModule != null)
+                    part.UpdateStageability(true, true);
+
+                //GameEvents.onEditorShipModified.Fire(EditorLogic.fetch.ship);
+                return;
+            }
+
+            SetStatus();
+            GameEvents.onEditorShipModified.Fire(EditorLogic.fetch.ship);
+            EditorLogic.fetch.SetBackup();
+            oldSepCnt = seperatronCnt;
+            GameEvents.onEditorShipModified.Add(onEditorShipModified);
+        }
+
+        void onEditorShipModified(ShipConstruct sc)
+        {
+            if (oldSepCnt != seperatronCnt)
+            {
+                oldSepCnt = seperatronCnt;
+                if (newNodes != null)
+                {
+                    newNodes.DelNodes(this.part);
+                    newNodes.AddNodes(this.part, (int)seperatronCnt);
+                }
+            }
+        }
+
+        private void OnDestroy()
+        {
+            GameEvents.onEditorShipModified.Remove(onEditorShipModified);
+        }
+
+        public override string GetInfo()
+        {
+            // need to add code here
+            Log.Info("GetInfo");
+            string s = "";
+            //if (techAvailable)
+            {
+                s = string.Format("Optional built-in decoupler\nOptional built-in Sepratrons");
+            }
+            Log.Info("GetInfo returning: " + s);
+            return s;
+        }
+
+        public float GetModuleMass(float m, ModifierStagingSituation mss)
+        {
+            if (integratedDecoupler)
+            {
+                return decouplerMass;
+            }
+            return 0;
+        }
+        public ModifierChangeWhen GetModuleMassChangeWhen()
+        {
+            return ModifierChangeWhen.CONSTANTLY;
+        }
+
+        public float GetModuleCost(float m, ModifierStagingSituation mss)
+        {
+            if (integratedDecoupler)
+            {
+                return decouplerCost;
+            }
+            return 0;
+        }
+        public ModifierChangeWhen GetModuleCostChangeWhen()
+        {
+            return ModifierChangeWhen.FIXED;
+
+        }
+
+        public bool DecouplerRequiresTech()
+        {
+            return !string.IsNullOrEmpty(this.techRequired);
+        }
+
+        public string DecouplerTech()
+        {
+            return this.techRequired;
+        }
+
+        public bool DecouplerHasTech()
+        {
+            return (HighLogic.CurrentGame.Mode != Game.Modes.CAREER && HighLogic.CurrentGame.Mode != Game.Modes.SCIENCE_SANDBOX) || 
+                ResearchAndDevelopment.GetTechnologyState(this.techRequired) == RDTech.State.Available;
+        }
+    }
+}
---
+++ Source/NodeControl.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace IntegratedStackedTankDecouplers
+{
+    public class NodeControl
+    {
+
+        AttachNode topNode;
+        public Vector3 pos;
+        const double RADIAN = 180 / Math.PI;
+
+        const string NODE_PREFIX = "sep_";
+
+
+        public void DelNodes(Part p)
+        {
+            Log.Info("Deleting Nodes");
+#if true
+            for (int idx = p.attachNodes.Count() - 1; idx >= 0; idx--)
+            {
+                if (p.attachNodes[idx].id.Length >= NODE_PREFIX.Length)
+                    if (p.attachNodes[idx].id.Substring(0, NODE_PREFIX.Length) == NODE_PREFIX)
+                        p.attachNodes.Remove(p.attachNodes[idx]);
+            }
+
+#else
+            for (int idx = p.children.Count() - 1; idx > 0; idx--)
+            {
+                var part = p.children[idx];
+                Log.Info("part.partInfo.name: " + part.partInfo.name);
+                if (part.partInfo.name == "integratedSepMotor")
+                {
+                    Log.Info("Deleting part");
+                   // p.removeChild(part);
+                   // EditorLogic.fetch.ship.Parts.Remove(part);
+                    EditorLogic.DeletePart(part);
+
+                }
+            }
+#endif
+        }
+
+        public NodeControl(Part p, int nodeCnt)
+        {
+            AddNodes(p, nodeCnt);
+        }
+
+        public void AddNodes(Part p, int nodeCnt)
+        {
+            Log.Info("Adding nodes");
+#if true
+            if (GetTopNode(p) && nodeCnt > 0)
+            {
+                DelNodes(p);
+                double radius = Math.Min(p.prefabSize.z / 2, p.prefabSize.x / 2);
+
+                double angle = 0;
+                for (int cnt = 0; cnt < Math.Max(2, nodeCnt); cnt++)
+                {
+                    angle = ((360 / nodeCnt) / RADIAN) * cnt;
+
+                    string newNodeName = NODE_PREFIX + cnt.ToString();
+
+
+                    double x = radius * Math.Cos(angle);
+                    double z = radius * Math.Sin(angle);
+                    //pos.z = pos.z + p.prefabSize.z / 2;
+
+                    pos = topNode.position;
+
+                    pos.x += (float)Math.Round(x, 4);
+                    pos.z += (float)Math.Round(z, 4);
+
+                    var an = new AttachNode
+                    {
+                        owner = p,
+                        position = pos,
+                        nodeType = AttachNode.NodeType.Stack,
+                        size = 0,
+                        id = newNodeName,
+                        attachMethod = AttachNodeMethod.FIXED_JOINT,
+                        nodeTransform = p.transform,
+                        orientation = p.transform.up
+                    };
+
+                    p.attachNodes.Add(an);
+                }
+            }
+#else
+            AddParts(p, nodeCnt);
+#endif
+        }
+
+        bool GetTopNode(Part p)
+        {
+            foreach (var attachNode in p.attachNodes.Where(an => an != null))
+            {
+                if (p.srfAttachNode != null && attachNode == p.srfAttachNode)
+                    continue;
+                if (attachNode.id == "")
+                    continue;
+                if (attachNode.id == "top")
+                {
+                    topNode = attachNode;
+                    return true;
+
+                }
+            }
+            return false;
+        }
+
+        public Part CreatePart(string partname)
+        {
+            AvailablePart avPart = PartLoader.getPartInfoByName(partname);
+
+            UnityEngine.Object obj = UnityEngine.Object.Instantiate(avPart.partPrefab);
+            if (!obj)
+            {
+                Log.Info("CreatePart(Crate) Failed to instantiate " + avPart.partPrefab.name);
+                return null;
+            }
+
+            Part newPart = UnityEngine.Object.Instantiate<Part>(avPart.partPrefab);
+
+            newPart.gameObject.SetActive(true);
+            newPart.gameObject.name = avPart.name;
+            newPart.partInfo = avPart;
+            newPart.symMethod = SymmetryMethod.Radial;
+    
+            return newPart;
+        }
+
+
+        void AddParts(Part p, int nodeCnt)
+        {
+            double radius = Math.Min(p.prefabSize.z / 2, p.prefabSize.x / 2);
+            double angle = 0;
+
+            if (GetTopNode(p) && nodeCnt > 0)
+            {
+                for (int cnt = 0; cnt < Math.Max(2, nodeCnt); cnt++)
+                {
+
+                    Part newP = CreatePart("integratedSepMotor");
+                    Log.Info("newP prefabSize.x: " + newP.prefabSize.x.ToString() + ", z: " + newP.prefabSize.z.ToString());
+                    radius = Math.Min(p.prefabSize.z / 2, p.prefabSize.x / 2) - Math.Max(newP.prefabSize.x, newP.prefabSize.z);
+                    if (newP != null)
+                    {
+                        //newP.partTransform = topNode.nodeTransform;
+                        angle = ((360 / nodeCnt) / RADIAN) * cnt;
+                        double x = radius * Math.Cos(angle);
+                        double z = radius * Math.Sin(angle);
+                        //pos.z = pos.z + p.prefabSize.z / 2;
+
+                        pos = topNode.position;
+                        pos.x += (float)Math.Round(x, 4);
+                        pos.z += (float)Math.Round(z, 4);
+
+                        Log.Info("cnt: " + cnt.ToString() + ",  angle: " + (angle * RADIAN).ToString() + ",   x: " + pos.x.ToString() + ", z: " + pos.z.ToString());
+
+                        // multiply rotation by 180 on y axis
+                       // newP.transform.localRotation *= Quaternion.Euler(0f, 0f, 180f);
+
+                        Log.Info("Adding part");
+                        EditorLogic.fetch.ship.Add(newP);
+                        p.addChild(newP);
+                        newP.setParent(p);
+                        newP.transform.parent = p.transform;
+
+                        newP.transform.localPosition = pos;
+
+                        newP.attRotation0 = newP.transform.localRotation;
+                        newP.attPos0 = pos + p.attPos;
+                        newP.onAttach(p, true);
+
+                    }
+                }
+                GameEvents.onEditorShipModified.Fire(EditorLogic.fetch.ship);
+            }
+
+        }
+    }
+}
+
